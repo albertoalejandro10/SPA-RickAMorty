dd3a63a1140b9b6e75cf047f37ea476521ca5174
Alberto Alejandro <alberto196g@gmail.com>
2020-08-31 23:43:39 -0400

    Conectando las rutas

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,44 @@
 const path = require('path'); // Nos permite acceder a donde estamos en las carpetas. Ya sea en local o en la nube.
 
-const HtmlWebpackPlugin = requre('html-webpack-plugin'); // Archivo necesario para trabjar con Html
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Archivo necesario para trabjar con Html
 
-Module.exports = { //Aqui se encuentra toda la configuracion de lo que va a suceder. Modulo para exportar
-    entry: './index.js', // Punto de entrada con su direccion. Aqui vive el codigo inicial y de aqui parte al desarrollo
+module.exports = { //Aqui se encuentra toda la configuracion de lo que va a suceder. Modulo para exportar
+
+    entry: './src/index.js', // Punto de entrada con su direccion. Aqui vive el codigo inicial y de aqui parte al desarrollo
+
     output: { // Donde se envia el proyecto estructurado y compilado listo para produccion
+
         path: path.resolve(__dirname, 'dist'), // Creamos el lugar donde se exportara el proyecto
+
         filename: 'main.js' //Este es el nombre del archivo final para produccion
     },
+
     resolve: {
-        extensions: ['/js'], // Extensiones que vamos a utilizar
+
+        extensions: ['.js'], // Extensiones que vamos a utilizar
+
     },
     module: { // Se crea un modulo con las reglas necesarias que vamos a utilizar
+
         rules: [ //Reglas
+
             {
                 test: /\,js?$/, //Nos permite identificar los archivos segun se encuentran en nuestro entorno.
+
                 exclude: /node_modules/, //Excluimos la carpeta de node_modules
+
                 use: {
+
                     loader: 'babel-loader', //Utilizar un loader como configuracion establecida
+
                 }
             }
         ]
     },
     
-    plugins: [ // Establecemos los plugins que amos a usar 
+    plugins: [ // Establecemos los plugins que vamos a usar 
 
-        new HtmlWebpackPlugin([ // Permite trabajar con los archivos html
+        new HtmlWebpackPlugin( // Permite trabajar con los archivos html
             {
                 inject: true, //Como amos a inyectar un valor a un archivo html
 
@@ -34,7 +47,7 @@ Module.exports = { //Aqui se encuentra toda la configuracion de lo que va a suce
                 filename: './index.html' //El nombre que tendra el archivo
 
             }
-        ])
+        )
 
     ]
 }
