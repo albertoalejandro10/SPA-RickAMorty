c7a242457376f95f82bffc7c5d703cad3c6bd12e
Alberto Alejandro <alberto196g@gmail.com>
2020-09-01 23:49:36 -0400

    Mejore el codigo para la extraccion de informacion

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -4,19 +4,22 @@ import getData from '../utils/getData';
 const Character = async () => {
     const id = getHash();
     const character = await getData(id);
+
+    const {image, name, episode, status, species, gender, origin, location} = character;
+
     const view = `
         <div class="Characters-in">
             <article class="Characters-card">
-                <img src="${character.image}" alt="${character.name}">
-                <h2>${character.name}</h2>
+                <img src="${image}" alt="${name}">
+                <h2>${name}</h2>
             </article>
             <article class="Characters-card">
-                <h3>Episodes: <span>${character.episode.length}</span></h3>
-                <h3>Status:  <span>${character.status}</span></h3>
-                <h3>Species: <span>${character.species}</span></h3>
-                <h3>Gender:  <span>${character.gender}</span></h3>
-                <h3>Origin:  <span>${character.origin}</span></h3>
-                <h3>Last Location: ${character.location.name}</h3>
+                <h3>Episodes: <span>${episode.length}</span></h3>
+                <h3>Status:  <span>${status}</span></h3>
+                <h3>Species: <span>${species}</span></h3>
+                <h3>Gender:  <span>${gender}</span></h3>
+                <h3>Origin:  <span>${origin}</span></h3>
+                <h3>Last Location: ${location.name}</h3>
             </article>
         </div>
     `
