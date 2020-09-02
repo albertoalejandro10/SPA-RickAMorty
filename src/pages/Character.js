de83971a37b3ee72776f3dad78ac47ae16a5ec6f
Alberto Alejandro <alberto196g@gmail.com>
2020-09-02 18:53:00 -0400

    Corrigiendo errores

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -8,7 +8,7 @@ const Character = async () => {
     const {image, name, episode, status, species, gender, origin, location} = character;
 
     const view = `
-        <div class="Characters-in">
+        <div class="Characters-inner">
             <article class="Characters-card">
                 <img src="${image}" alt="${name}">
                 <h2>${name}</h2>
@@ -18,7 +18,7 @@ const Character = async () => {
                 <h3>Status:  <span>${status}</span></h3>
                 <h3>Species: <span>${species}</span></h3>
                 <h3>Gender:  <span>${gender}</span></h3>
-                <h3>Origin:  <span>${origin}</span></h3>
+                <h3>Origin:  <span>${origin.name}</span></h3>
                 <h3>Last Location: ${location.name}</h3>
             </article>
         </div>
